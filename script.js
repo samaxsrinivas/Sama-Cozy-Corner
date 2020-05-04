aee42da00cd63415c3b46adeb1bb00963ef80d5c
samaxsrinivas <sama.saxena.srinivas@gmail.com>
2020-05-04 12:48:41 -0700

    on click popup show API data

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("listBooks");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function myFunction() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function myFunction() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function myFunction(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 const app = {
 
     books: [],
 
     initialize: function() {
-        $('.listBooks').click(function() {
+        $('#listBooks').click(function() {
             app.fetchBooks();
         });
 
@@ -20,7 +46,7 @@ const app = {
             theHTML += "<p>" + app.books[i].volumeInfo.description + "</p>";
             theHTML += "</div>";
         }
-        $('.container').html(theHTML);
+        $('.modal-content').html(theHTML);
     },
     fetchBooks: function() {
 
@@ -40,7 +66,7 @@ const app = {
                 console.log(err);
             },
             success: function(data) {
-                console.log(data);
+                // console.log(data);
                 app.books = data.items;
                 app.makeHTML(data);
             }
