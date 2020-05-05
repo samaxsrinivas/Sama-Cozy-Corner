1e1e576612ca7a510e582a7584d92af838606922
samaxsrinivas <sama.saxena.srinivas@gmail.com>
2020-05-04 19:29:12 -0700

    adding ui

--- script.js
+++ script.js
@@ -1,77 +1,53 @@
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("listBooks");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal 
-// btn.onclick = function myFunction() {
-//     modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function myFunction() {
-//     modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function myFunction(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
 const app = {
 
     books: [],
 
     initialize: function() {
-        $('#listBooks').click(function() {
+        $('.popup').click(function() {
             app.fetchBooks();
         });
 
     },
 
-    imageAppend: function() {
-        const data = items;
-        const images = $(document.getElementsByClassName("popup")).findAll()
-        images.forEach(('.book1', data[0]) => {
-            $('.book1').click(() => {
-                // Get the modal
-                var modal = document.getElementById("myModal");
+    // imageAppend: function() {
+    //     const data = app.books[i];
+    //     const images = $(document.getElementsByClassName("popup")).findAll();
+    //     images.forEach((image, index) => {
+    //         $(image).click(() => {
+    //             if (images[i] == data[index]) {
+    //                 // Get the modal
+    //                 var modal = document.getElementById("myModal");
 
-                // Get the button that opens the modal
-                var btn = document.getElementById("listBooks");
+    //                 // Get the button that opens the modal
+    //                 var btn = document.getElementById("listBooks");
 
-                // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
+    //                 // Get the <span> element that closes the modal
+    //                 var span = document.getElementsByClassName("close")[0];
 
-                // When the user clicks the button, open the modal 
-                btn.onclick = function myFunction() {
-                    modal.style.display = "block";
-                }
+    //                 // When the user clicks the button, open the modal 
+    //                 btn.onclick = function myFunction() {
+    //                     modal.style.display = "block";
+    //                 }
 
-                // When the user clicks on <span> (x), close the modal
-                span.onclick = function myFunction() {
-                    modal.style.display = "none";
-                }
+    //                 // When the user clicks on <span> (x), close the modal
+    //                 span.onclick = function myFunction() {
+    //                     modal.style.display = "none";
+    //                 }
 
-                // When the user clicks anywhere outside of the modal, close it
-                window.onclick = function myFunction(event) {
-                    if (event.target == modal) {
-                        modal.style.display = "none";
-                    }
-                }
-            });
-        });
-    },
+    //                 // When the user clicks anywhere outside of the modal, close it
+    //                 window.onclick = function myFunction(event) {
+    //                     if (event.target == modal) {
+    //                         modal.style.display = "none";
+    //                     }
+    //                 }
+    //             }
+    //         })
+    //     });
+    // },
 
     makeHTML: function() {
         var theHTML = '';
-        for (var i = 0; i < app.books.length; i++) {
+        for (var i = 0; i < 1; i++) {
             theHTML += "<div class='books'>";
             theHTML += "<div class='left'>";
             theHTML += "<img class='bookCover' src='" + app.books[i].volumeInfo.imageLinks.thumbnail + "'>";
@@ -105,12 +81,39 @@ const app = {
                 console.log(err);
             },
             success: function(data) {
-                console.log(data);
+                // console.log(data);
                 app.books = data.items;
                 app.makeHTML(data);
-                app.imageAppend(data);
             }
         })
     },
 
+}
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("listBooks");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function myFunction() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function myFunction() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function myFunction(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
