34d3ee3d7c6e20dfce9d0efb00d07fde909181a5
samaxsrinivas <sama.saxena.srinivas@gmail.com>
2020-05-04 21:39:07 -0700

    popup triggered by multiple buttons

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const app = {
     books: [],
 
     initialize: function() {
-        $('.popup').click(function() {
+        $(".popup").click(function() {
             app.fetchBooks();
         });
 
@@ -24,7 +24,6 @@ const app = {
             theHTML += "</div>";
         }
         $('.modal-content').html(theHTML);
-
     },
 
     // imageAppend: function() {
@@ -34,23 +33,6 @@ const app = {
     //     function myFunction() {
     //         document.getElementsByClassName("books")[0].innerHTML = data[1];
     //     }
-    // },
-
-    // makeHTML2: function() {
-    //     var theHTML = '';
-    //     for (var i = 1; i < 2; i++) {
-    //         theHTML += "<div class='books'>";
-    //         theHTML += "<div class='left'>";
-    //         theHTML += "<img class='bookCover' src='" + app.books[i].volumeInfo.imageLinks.thumbnail + "'>";
-    //         theHTML += "</div>";
-    //         theHTML += "<div class='right'>";
-    //         theHTML += "<h3 class='bookTitle'>" + app.books[i].volumeInfo.title + "</h3>";
-    //         theHTML += "<h5 class='author'>" + "By " + app.books[i].volumeInfo.authors + "</h5>";
-    //         theHTML += "<p class='description'>" + app.books[i].volumeInfo.description + "</p>";
-    //         theHTML += "</div>";
-    //         theHTML += "</div>";
-    //     }
-    //     $('.modal-content').html(theHTML);
     // },
 
 
@@ -82,32 +64,22 @@ const app = {
 }
 
 
+$(document).on('click', '.popup', function() {
+    var myTargetModal = $(this).attr('href');
+    $('#myModal').hide();
+    $('.modal-content').hide();
 
-// Get the modal
-var modal = document.getElementById("myModal");
+    $('#myModal').fadeIn();
+    $(myTargetModal).fadeIn();
+    return false;
+});
 
-// Get the button that opens the modal
-// var btn = document.getElementById("listBooks");
-var btn = document.getElementsByClassName("popup")[0];
-var btn = document.getElementsByClassName("popup")[1];
+$("body").on("click", ".close", function() {
+    $('#myModal').hide();
+    $('.modal-content').hide();
+});
 
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function myFunction() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function myFunction() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function myFunction(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+$("body").on("click", ".mainBody", function() {
+    $('#myModal').hide();
+    $('.modal-content').hide();
+});
